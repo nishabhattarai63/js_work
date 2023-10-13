aefd00955c4126af4a10f43fe36f5d4ef62c2cf2
Nisha <nishabhattarai63@gmail.com>
2023-10-13 12:25:45 +0545

    this is js work

---
+++ main.js
@@ -0,0 +1,45 @@
+// //how to use external css
+// console.log("hello world");
+// let a="pratik";
+// let b=a.slice(0,-1);
+// console.log(b);
+
+
+//method chaining
+// let names ="pratik";
+// let letter= names.charAt(1).toLowerCase();
+// let character= names.charAt(4).toUpperCase();
+// console.log(letter);
+// console.log(character);
+
+// // if statement
+// let a=true;
+// if(a==true){
+//     console.log("this is true");
+// }
+// let b=false;
+// if(b==false){
+//     console.log("this is false");
+
+    // let c=17;
+    // if (c>18) {
+    //     console.log("higher than 18");
+    // }
+    // else if (c==18)
+    // {
+    //     console.log("equal to 18");
+    // }
+    // else
+    // {
+    //     console.log("lower than 18");
+    // }
+    // let n;
+    // n=window.prompt("enter your age");
+    // if(n >=18){
+    //     console.log("you are eligible");
+    // }
+    // else{
+    //     console.log("you are not");
+    // }
+
+   
